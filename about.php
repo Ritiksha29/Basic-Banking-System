d8981534feda69c1bd607c0fe6074c439d11899a
Ritiksha29 <98871125+Ritiksha29@users.noreply.github.com>
2022-02-20 11:17:20 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,44 @@
+<?php include('nav.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us Section</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+	<link rel="stylesheet" type="text/css" href="css/about.css">
+</head>	
+<body>
+	<div class="section">
+		<div class="container">
+			<div class="content-section">
+				<div class="title">
+					<h1>About Us</h1>
+				</div>
+				<div class="content">
+					
+					<p>Easy and quick transfer and maximum customer satisfaction.We allow you to view all customers details and hope that you will respect the privacy of each  
+                    user.Transaction history is also accessible to each customer sa as to ensure data transparency as well as the credebility 
+					of our bank. That being said,please free to explore our newly updated site wherein you can make transaction anytime,anywhere.
+
+					</p>
+					<!-- <div class="button">
+						<a href="">Read More</a>
+					</div> -->
+				</div>
+				<!-- <div class="social"> -->
+					<!-- <a href=""><i class="fab fa-facebook-f"></i></a> -->
+					<!-- <a href=""><i class="fab fa-twitter"></i></a> -->
+					<!-- <a href=""><i class="fab fa-instagram"></i></a> -->
+				<!-- </div> -->
+			</div>
+			<div class="image-section">
+				<img src="https://img.freepik.com/free-vector/banking-related-icons-image_24911-12415.jpg?size=626&ext=jpg">
+			</div>
+		</div>
+	</div>
+
+
+</body>
+</html>
+<?php include('footer.php'); ?>	
---
+++ footer.php
@@ -0,0 +1,5 @@
+<div id="container">
+    <div id="header">© 2022. Made by 
+Ritiksha Tipre<br>
+      For the Project of The Sparks Foundation</div>
+ </div>
